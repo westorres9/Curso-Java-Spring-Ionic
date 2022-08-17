e0f5ade5ac3a1f3e04148c3b92446e111eec0553
westorres9 <wester.torres9@gmail.com>
2022-08-17 08:29:03 -0300

    projeto criado

---
+++ backend/src/test/java/com/devsuperior/springionic/SpringionicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringionicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
