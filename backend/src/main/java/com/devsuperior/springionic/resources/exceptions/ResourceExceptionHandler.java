56d88abc1fc861556e8e74b0be6021398ee05de7
westorres9 <wester.torres9@gmail.com>
2022-08-17 11:57:23 -0300

    ResourceExceptionHandler

--- backend/src/main/java/com/devsuperior/springionic/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/springionic/resources/exceptions/ResourceExceptionHandler.java
@@ -1,8 +1,31 @@
 package com.devsuperior.springionic.resources.exceptions;
 
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.springionic.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError();
+		err.setStatus(status.value());
+		err.setError("Resource Not Found Exception");
+		err.setMessage(e.getMessage());
+		err.setTimestamp(Instant.now());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+		
+	}
 
 }
