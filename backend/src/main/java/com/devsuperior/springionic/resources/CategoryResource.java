b27a7a7c1f5de80881b0647dc1d8d9bd133a3aff
westorres9 <wester.torres9@gmail.com>
2022-08-17 09:22:13 -0300

    categoryDTO, categoryServicefindById

--- backend/src/main/java/com/devsuperior/springionic/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/springionic/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.springionic.entities.Category;
+import com.devsuperior.springionic.dto.CategoryDTO;
 import com.devsuperior.springionic.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
     @GetMapping
-    public ResponseEntity<Page<Category>> findAll(Pageable pageable) {
-    	Page<Category> page = service.findAll(pageable);
+    public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+    	Page<CategoryDTO> page = service.findAll(pageable);
     	return ResponseEntity.ok().body(page);
     }
 
--- backend/src/main/java/com/devsuperior/springionic/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/springionic/services/CategoryService.java
@@ -1,12 +1,18 @@
 package com.devsuperior.springionic.services;
 
-import com.devsuperior.springionic.entities.Category;
-import com.devsuperior.springionic.repositories.CategoryRepository;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.springionic.dto.CategoryDTO;
+import com.devsuperior.springionic.entities.Category;
+import com.devsuperior.springionic.repositories.CategoryRepository;
 
 @Service
 public class CategoryService {
@@ -15,9 +21,16 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public Page<Category> findAll(Pageable pageable) {
+    public Page<CategoryDTO> findAll(Pageable pageable) {
         Page<Category> page = repository.findAll(pageable);
-        return page;
+        return page.map(x -> new CategoryDTO(x));
+    }
+    
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+    	Optional<Category> obj = repository.findById(id);
+    	Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not found"));
+    	return new CategoryDTO(entity);
     }
 
 }
